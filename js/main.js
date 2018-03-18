cfc7b0b08ffa9d95ec1e67abb402b5bf0e866985
edyta <edyta.daszkowska@gmail.com>
2018-03-18 22:18:22 +0100

    [#21] adds changes after code review

--- js/main.js
+++ js/main.js
@@ -10,17 +10,31 @@ $(document).ready(function(){
     });
 });
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("myBtn").style.display = "block";
+//     } else {
+//         document.getElementById("myBtn").style.display = "none";
+//     }
+// }
 
-function topFunction() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
-}
+// function topFunction() {
+//     document.body.scrollTop = 0; 
+//     document.documentElement.scrollTop = 0; 
+// }
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#myBtn').fadeIn(); 
+        } else { 
+            $('#myBtn').fadeOut(); 
+        } 
+    }); 
+    $('#myBtn').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+ });
